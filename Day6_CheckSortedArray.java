ef673483d519a482f6e41b41e6bb9906334ec472
Subhash <senagapallisubhash@gmail.com>
2026-03-13 18:50:02 +0530

    Day 6 DSA - Check if Array is Sorted

---
+++ Day6_CheckSortedArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class CheckSortedArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        boolean sorted = true;
+
+        for(int i = 0; i < n - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                sorted = false;
+                break;
+            }
+        }
+
+        if(sorted){
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
+}
