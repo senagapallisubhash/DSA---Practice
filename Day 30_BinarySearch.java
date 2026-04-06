feceb652a096dee22fb0a133983963fad742b928
Subhash <senagapallisubhash@gmail.com>
2026-04-06 22:25:20 +0530

    Day 30_BinarySearch.java

---
+++ Day 30_BinarySearch.java
@@ -0,0 +1,32 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+        int target = 23;
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // search right half
+            } else {
+                right = mid - 1; // search left half
+            }
+        }
+
+        return -1; // not found
+    }
+}
