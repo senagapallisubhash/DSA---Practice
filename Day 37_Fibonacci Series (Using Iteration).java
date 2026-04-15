73aad656c4c7db3f7424442549e6cbeb9f344457
Subhash <senagapallisubhash@gmail.com>
2026-04-15 11:04:29 +0530

    Day 37_Fibonacci Series (Using Iteration).java

---
+++ Day 37_Fibonacci Series (Using Iteration).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
