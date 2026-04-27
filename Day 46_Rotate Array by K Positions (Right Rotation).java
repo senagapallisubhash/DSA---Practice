6098f6b41f43e842f70a63d63daef52213dfeacd
Subhash <senagapallisubhash@gmail.com>
2026-04-27 19:29:38 +0530

    Day 46_Rotate Array by K Positions (Right Rotation).java

---
+++ Day 46_Rotate Array by K Positions (Right Rotation).java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotateRight(arr, k);
+
+        System.out.println("Rotated Array: " + Arrays.toString(arr));
+    }
+
+    static void rotateRight(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    static void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
