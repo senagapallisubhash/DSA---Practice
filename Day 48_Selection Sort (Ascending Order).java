daa61f28a6c057dfe4c67143a761542e20d010fe
Subhash <senagapallisubhash@gmail.com>
2026-05-01 10:04:21 +0530

    Day 48_Selection Sort (Ascending Order).java

---
+++ Day 48_Selection Sort (Ascending Order).java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SelectionSortExample {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
