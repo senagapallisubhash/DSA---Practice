a1de76ad8f1c76e931c2e04dcc8ef01a38bbc67f
Subhash <senagapallisubhash@gmail.com>
2026-05-12 19:44:50 +0530

    Day 54_Two Sum Problem (Using HashMap).java

---
+++ Day 54_Two Sum Problem (Using HashMap).java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        findTwoSum(arr, target);
+    }
+
+    static void findTwoSum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                System.out.println("Indices: " 
+                        + map.get(complement) + " and " + i);
+                return;
+            }
+
+            map.put(arr[i], i);
+        }
+
+        System.out.println("No pair found.");
+    }
+}
