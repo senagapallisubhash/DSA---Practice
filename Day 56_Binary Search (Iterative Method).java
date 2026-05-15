a484c933a1d5163aa32c44a3d5db8e63af873ec4
Subhash <senagapallisubhash@gmail.com>
2026-05-15 20:25:50 +0530

    Day 56_Binary Search (Iterative Method).java

---
+++ Day 56_Binary Search (Iterative Method).java
@@ -0,0 +1,38 @@
+public class BinarySearchExample {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+        int target = 23;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+
+    static int binarySearch(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
