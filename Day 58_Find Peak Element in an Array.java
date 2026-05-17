f7f3e0f8083b18c0d8fd316761aabb6775f7d57c
Subhash <senagapallisubhash@gmail.com>
2026-05-17 12:02:46 +0530

    Day 58_Find Peak Element in an Array.java

---
+++ Day 58_Find Peak Element in an Array.java
@@ -0,0 +1,31 @@
+public class PeakElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 20, 4, 1, 0};
+
+        int index = findPeak(arr);
+
+        System.out.println("Peak Element: " + arr[index]);
+        System.out.println("Peak Index: " + index);
+    }
+
+    static int findPeak(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
